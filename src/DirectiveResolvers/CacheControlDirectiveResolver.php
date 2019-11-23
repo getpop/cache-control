44b20a32679e9f5bd3e3dae6a1387d798d53041d
Leo <losoviz@gmail.com>
2019-11-23 22:20:34 +0800

    Improved description in schema

--- src/DirectiveResolvers/CacheControlDirectiveResolver.php
+++ src/DirectiveResolvers/CacheControlDirectiveResolver.php
@@ -2,9 +2,24 @@
 namespace PoP\CacheControl\DirectiveResolvers;
 
 use PoP\CacheControl\Environment;
+use PoP\Translation\Facades\TranslationAPIFacade;
+use PoP\ComponentModel\FieldResolvers\FieldResolverInterface;
 
 class CacheControlDirectiveResolver extends AbstractCacheControlDirectiveResolver
 {
+    public function getSchemaDirectiveDescription(FieldResolverInterface $fieldResolver): ?string
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        return sprintf(
+            $translationAPI->__('%1$s %2$s'),
+            sprintf(
+                $translationAPI->__('Default \'%1$s\' directive, used if no other %1$s directive can process the affected fields.', 'cache-control'),
+                $this::getDirectiveName()
+            ),
+            parent::getSchemaDirectiveDescription($fieldResolver)
+        );
+    }
+
     /**
      * The default max-age is configured through an environment variable
      *
