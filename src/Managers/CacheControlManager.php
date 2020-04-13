12aac4a08aa5cf9b8ac5149a293c392dbeb8f488
Leo <losoviz@gmail.com>
2020-04-13 19:09:03 +0800

    Added declare(strict_types=1);

--- src/Managers/CacheControlManager.php
+++ src/Managers/CacheControlManager.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\CacheControl\Managers;
 
 use PoP\CacheControl\Managers\CacheControlManagerInterface;
--- src/TypeResolverDecorators/ConfigurableCacheControlTypeResolverDecoratorTrait.php
+++ src/TypeResolverDecorators/ConfigurableCacheControlTypeResolverDecoratorTrait.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\CacheControl\TypeResolverDecorators;
 
 use PoP\CacheControl\DirectiveResolvers\AbstractCacheControlDirectiveResolver;
